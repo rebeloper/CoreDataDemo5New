277380babbc87ad1c91e2aa02842009b01108cdc
Alex Nagy <therebeloper@gmail.com>
2020-07-17 09:57:00 +0300

    Initial Commit

---
+++ CoreDataDemo5New/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo5New
+//
+//  Created by Alex Nagy on 17/07/2020.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
